9e4c171a4284763a278aad045db23b41239ad37c
jonkavana <G00387868@gmit.ie>
2020-05-04 18:08:26 +0100

    First iterations of high level analysis added to script.

--- Analysis.py
+++ Analysis.py
@@ -5,4 +5,17 @@ import matplotlib as plt
 
 
 IrisData = pandas.read_csv('IRIS.csv')
-print(IrisData)
+
+# print(IrisData)
+# this has output the full 150 instances of the data set to the terminal. 
+
+# to provide an analysis of the information within the text, we are going to extract the following from the data: 
+# Head of the data, tail of the data, describe of all the data contained. 
+
+# a = IrisData.head
+# print(a(10))
+# this will provide a cursory glance at he material from the first 10 rows of data.
+
+z = IrisData.describe
+print(z())
+
